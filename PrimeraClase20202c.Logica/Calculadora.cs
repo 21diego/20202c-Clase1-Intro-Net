24414c6c869e0cd4f15190f3086434d0de0d8f0c
psanchez <psanchez@plus54.com>
2020-09-02 22:33:49 -0300

    prueba conflictos

--- PrimeraClase20202c.Logica/Calculadora.cs
+++ PrimeraClase20202c.Logica/Calculadora.cs
@@ -8,9 +8,16 @@ namespace PrimeraClase20202c.Logica
 {
     public class Calculadora
     {
+        /// <summary>
+        /// Sumar
+        /// </summary>
+        /// <param name="input1"></param>
+        /// <param name="input2"></param>
+        /// <returns></returns>
         public static int Sumar(string input1, string input2)
         {
             int num1 = Convert.ToInt32(input1);
+            int num3 = 10 * num1;
             int num2 = Convert.ToInt32(input2);
 
             return num1 + num2;
